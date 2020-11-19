a0589295ffe809d3629a2f9e87d0df4d834b2157
soospitz <sks568@nyu.edu>
2020-11-19 11:51:36 -0500

    added about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,17 @@ import "../App.css";
 function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>ABOUT</h1>
+      <div className="about">
+        <p>
+          This project was built for practice purposes using React.
+        </p>
+        <a href="https://github.com/soospitz/shopping-app">
+          <button className="button">
+            VISIT REPO<i class="fab fa-github fa-lg"></i>
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
